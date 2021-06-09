8ed375ccabaf92fb1b9c46e31fe5e8a10106db98
AjayNaruka <naruka.ajay1997@gmail.com>
2021-06-09 13:17:29 +0200

    fine

--- api.php
+++ api.php
@@ -1,5 +1,41 @@
 <?php 
   include __DIR__ . '/data/db.php';
+ 
+  $genres=[];
+  $filtered=[];
+  $options=[];
+  $key=$_GET['key'];
+  $toSearch=$_GET['value'];
+  $options= array_keys($database[0]);
+  $options = array_diff($options,['poster']);
+
+
+  if($toSearch==='all' || $toSearch===''){
+    $filtered=$database;
+  }else{
+    foreach($database as $album){
+      if((strpos(strtolower($album[$key]),strtolower($toSearch))===0)||(strpos(strtolower($album[$key]),strtolower($toSearch))>0)){
+        $filtered[]=$album;
+      }
+    }
+  }
+   
+
+  /* ORDINO ARRAY PER DATA CRESCENTE */
+  if(count($filtered)>0){
+    $year = array_column($filtered, 'year');
+    array_multisort($year, SORT_DESC, $filtered);
+  }
+  
+  
+
+  $response =[
+    'albums'=> $filtered,
+    'genres'=> $genres,
+    'options'=>$options
+  ];
+
+
   header('Content-Type: application/json'); // devo specificare il tipo
-  echo json_encode($database);
+  echo json_encode($response);
 ?>
--- main.js
+++ main.js
@@ -4,20 +4,44 @@ const app = new Vue({
     test:false,
     title:'TITOLO',
     dischi:[],
+    toSearchGenre:'',
+    selected:'title',
+    genres:[], /* POSSIBILI GENERI */
+    opzioni:[]
 
   },
   created(){
-    axios.get('http://localhost:80/Projects/php-ajax-dischi/api.php')
+    this.apiCall('all','title');
+  },
+  methods:{
+    
+    apiCall(str,chiave){
+      console.log('filtro per : ', this.selected);
+      console.log('tipo: ', typeof(this.selected));
+      console.log('sto cercando: ', str);
+      axios.get('http://localhost:80/Projects/php-ajax-dischi/api.php',
+    {
+      params:{
+        value :str,
+        key: chiave
+      }
+    })
     .then(res => {
       console.log('ciao');
-      this.dischi=res.data;
+      this.genres=res.data.genres;
+      this.dischi=res.data.albums;
+      this.opzioni=res.data.options
       console.log(this.dischi);
+      console.log(this.genres);
+      console.log(this.opzioni);
     })
     .catch(err => {
       console.error('errore'); 
     })
-  },
-  methods:{
-
+    },
+     capitalizeFirstLetter(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+    
   }
 })
